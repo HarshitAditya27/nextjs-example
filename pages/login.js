6d6fc2bda1cf5ed89007cc00127082a896028f40
Harshit <harshitaditya27@gmail.com>
2022-02-17 19:28:52 +0530

    Added Files

---
+++ pages/login.js
@@ -0,0 +1,44 @@
+import Head from 'next/head'
+import React, { useEffect } from "react";
+import SawoLogin from "sawo-react"
+import lstyles from '../styles/login.module.css'
+function login() {
+    function sawoLoginCallback(payload) {
+        console.log(payload);
+    }
+
+    const sawoConfig = {
+        onSuccess: (payload) => {
+            console.log(payload)
+            window.location.href = "/";
+        },
+        identifierType: "email", //required, must be one of: 'email', 'phone_number_sms',
+        apiKey: "d7542d08-230b-48b0-a465-2a2a444cce4f", // required, get it from sawo dev.sawolabs.com,
+        containerHeight: "300px", // the login container height, default is 300px
+    };
+
+    return (
+
+        <div class={lstyles.container}>
+            <Head>
+                <title>Login</title>
+            </Head>
+            <div class={lstyles.login_container}>
+                <h1>Login</h1>
+                <SawoLogin config={sawoConfig} />
+            </div>
+        </div>
+
+
+    );
+};
+
+export default login;
+
+/*       <div class={lstyles.container}>
+            <div class={lstyles.login_container}>
+                
+            </div>
+        </div>
+
+*/
